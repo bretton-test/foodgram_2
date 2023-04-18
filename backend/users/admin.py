b97235f680be3dd561360f3b1713d6c98cc2898e
Alex Dolganov <alex.dolganov@gmail.com>
2023-04-18 22:01:08 +0500

    user admin add

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from .models import Follow
+
+User = get_user_model()
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'author')
+    list_display_links = ('user',)
+    list_filter = ('user',)
+    search_fields = ('user', 'author')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_filter = ('username', 'email')
+    search_fields = ('username', 'email')
